d8a3865149026ed5502df137f7fb254a035e8b40
Magi <paoloimbrmagi@gmail.com>
2024-11-13 14:31:35 +0100

    Updated Algo and Reti

---
+++ Second Year/Algoritmi/OrderingAlgorithms/src/stack.h
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stack
+{
+    int val;
+    stack* next;
+}stack;
+
+stack* push(stack* top, int val)
+{
+    // Alloco spazio per il nuovo elemento
+    stack* new_top = (stack*)malloc(sizeof(stack));
+
+    if(top != NULL) {
+        new_top->val = val;
+        new_top->next = top;
+        top = new_top;
+    } else {
+        printf("Memoria esaurita");
+    }
+
+    return new_top;
+}
+
+stack* pop(stack* top)
+{
+    if(top != NULL) {
+        stack* temp = top;
+        top = top->next;
+        free(temp);
+    } else {
+        printf("Stack vuoto");
+    }
+
+    return top;
+}
